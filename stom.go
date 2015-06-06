7a3f3a122a3036d786a2c2a5dee21226482d686b
elgris <iddqd07@yandex.ru>
2015-06-06 16:35:13 +0700

    - removed unused struct from tests - added test structures for future use - modified test cases for better debugging - moved benchmarks to separate file - removed support of multiple tags: they add more problems than use - added ToMappable interface

--- stom.go
+++ stom.go
@@ -1,5 +1,12 @@
 package stom
 
+import (
+	"database/sql/driver"
+	"errors"
+	"fmt"
+	"reflect"
+)
+
 type Policy uint8
 
 const (
@@ -9,21 +16,20 @@ const (
 	defaultTag = "db"
 )
 
-var defaultStom = stom{[]string{defaultTag}, PolicyUseDefault, nil}
+type ToMappable interface {
+	ToMap() (map[string]interface{}, error)
+}
+
+var defaultStom = stom{defaultTag, PolicyUseDefault, nil}
 
 type stom struct {
-	tags         []string
+	tag          string
 	policy       Policy
 	defaultValue interface{}
 }
 
-func (this *stom) SetTags(tags ...string) {
-	if len(tags) == 0 {
-		this.tags = []string{defaultTag}
-		return
-	}
-
-	this.tags = tags
+func (this *stom) SetTag(tag string) {
+	this.tag = tag
 }
 
 func (this *stom) SetDefault(defaultValue interface{}) {
@@ -34,18 +40,81 @@ func (this *stom) SetPolicy(policy Policy) {
 	this.policy = policy
 }
 
-func SetDefault(defaultValue interface{}) {
-	defaultStom.SetDefault(defaultValue)
+func (this *stom) ToMap(s interface{}) (map[string]interface{}, error) {
+	typ := reflect.TypeOf(s)
+	// if a pointer to a struct is passed, get the type of the dereferenced object
+	if typ.Kind() == reflect.Ptr {
+		typ = typ.Elem()
+	}
+
+	if typ.Kind() != reflect.Struct {
+		return nil, errors.New(fmt.Sprintf("expected struct, got %v", typ.Kind()))
+	}
+
+	result := map[string]interface{}{}
+
+	val := reflect.ValueOf(s)
+	// loop through the struct's fields and set the map
+	for i := 0; i < typ.NumField(); i++ {
+		field := typ.Field(i)
+		if field.PkgPath != "" { // unexported
+			continue
+		}
+
+		// TODO: try indices instead of values
+		// TODO: parallelize it
+		// TODO: cache should work as so:
+		// 1. Create a map 'field name' => index
+		// 2. Get a value by index
+		// 3. Convert value to appropriate value
+		// 4. DONE
+
+		if t := field.Tag.Get(this.tag); t != "" && t != "-" {
+			vField := val.Field(i)
+			var v interface{}
+			if vField.Kind() == reflect.Ptr {
+				if vField.Elem().IsValid() {
+					v = vField.Elem().Interface()
+				}
+			} else {
+				v = vField.Interface()
+			}
+
+			v = convertValue(v)
+
+			if v != nil {
+				result[t] = v
+			} else if this.policy == PolicyUseDefault {
+				result[t] = this.defaultValue
+			}
+		}
+
+	}
+
+	return result, nil
+
+	// TODO:
+	// 1. Check if it's a struct. If not - return error
+	// 2. Scan through struct's fields and get all the tags
+	// 3. If tag is in this.tags - use tag name as a key and field value as value
+	// and put them into map
 }
 
-func SetTags(tags ...string) {
-	defaultStom.SetTags(tags...)
-}
+func SetDefault(defaultValue interface{})                 { defaultStom.SetDefault(defaultValue) }
+func SetTag(tag string)                                   { defaultStom.SetTag(tag) }
+func SetPolicy(policy Policy)                             { defaultStom.SetPolicy(policy) }
+func ToMap(s interface{}) (map[string]interface{}, error) { return defaultStom.ToMap(s) }
 
-func SetPolicy(policy Policy) {
-	defaultStom.SetPolicy(policy)
-}
+func convertValue(input interface{}) (output interface{}) {
+	// TODO: check if input is a structure
+	output = input
+	switch t := input.(type) {
+	case driver.Valuer:
+		if converted, err := t.Value(); converted == nil || err != nil {
+			output = nil
+		}
+	}
 
-func ToMap(s interface{}) map[string]interface{} {
-	return map[string]interface{}{}
+	return
+
 }
--- stom_test.go
+++ stom_test.go
@@ -10,28 +10,47 @@ import (
 )
 
 type SomeItem struct {
-	ID         int             `db:"id" custom_tag:"id"`
-	Name       string          `db:"name"`
-	Number     int             `db:"number" custom_tag:"num"`
-	Checksum   int32           `custom_tag:"sum"`
-	Created    time.Time       `custom_tag:"created_time" db:"created"`
-	Updated    mysql.NullTime  `db:"updated" custom_tag:"updated_time"`
-	Price      float64         `db:"price"`
-	Discount   *float64        `db:"discount"`
-	IsReserved sql.NullBool    `db:"reserved" custom_tag:"is_reserved"`
-	Points     sql.NullInt64   `db:"points"`
-	Rating     sql.NullFloat64 `db:"rating"`
-	IsVisible  bool            `db:"visible" custom_tag:"visible"`
-	Notes      string
+	ID              int             `db:"id" custom_tag:"id"`
+	Name            string          `db:"name"`
+	somePrivate     string          `db:"some_private" custom_tag:"some_private"`
+	Number          int             `db:"number" custom_tag:"num"`
+	Checksum        int32           `custom_tag:"sum"`
+	Created         time.Time       `custom_tag:"created_time" db:"created"`
+	Updated         mysql.NullTime  `db:"updated" custom_tag:"updated_time"`
+	Price           float64         `db:"price"`
+	Discount        *float64        `db:"discount"`
+	IsReserved      sql.NullBool    `db:"reserved" custom_tag:"is_reserved"`
+	Points          sql.NullInt64   `db:"points"`
+	Rating          sql.NullFloat64 `db:"rating"`
+	IsVisible       bool            `db:"visible" custom_tag:"visible"`
+	SomeIgnoreField int             `db:"-" custom_tag:"i_ignore_nothing"`
+	Notes           string
 }
 
-type testSet struct {
-	Item     SomeItem
-	Expected map[string]interface{}
+type BasicItem struct {
+	Base string `tomap:"base"`
 }
 
-func TestDefaultTag_DefaultPolicy_DefaultValue(t *testing.T) {
-	SetTags("db")
+type AnotherBasicItem struct {
+	AnotherBase string `tomap:"another_base"`
+}
+
+type ComplexItem struct {
+	SomeItem
+	BasicItem
+	AnotherBasicItem `tomap:"-"`
+	Author           sql.NullString `tomap:"author"`
+	Generation       uint32
+	Meta             struct {
+		Tag        string `tomap:"tag"`
+		Value      string `tomap:"-"`
+		SomeFlag   bool
+		Additional map[string]interface{} `tomap:"additional"`
+	}
+}
+
+func TestDefaultPolicy_DefaultValue(t *testing.T) {
+	SetTag("db")
 	SetDefault("DEFAULT")
 	SetPolicy(PolicyUseDefault)
 
@@ -50,25 +69,25 @@ func TestDefaultTag_DefaultPolicy_DefaultValue(t *testing.T) {
 			"visible":  true,
 		},
 		map[string]interface{}{
-			"id":       1,
-			"name":     "item_1",
-			"number":   11,
-			"created":  time.Unix(10000, 0),
+			"id":       2,
+			"name":     "item_2",
+			"number":   22,
+			"created":  time.Unix(20000, 0),
 			"updated":  "DEFAULT",
 			"discount": "DEFAULT",
-			"price":    1111.0,
+			"price":    2222.0,
 			"reserved": "DEFAULT",
 			"points":   "DEFAULT",
 			"rating":   "DEFAULT",
-			"visible":  true,
+			"visible":  false,
 		},
 	}
 
 	doTest(t, getTestItems(), expecteds)
 }
 
-func TestDefaultTag_DefaultPolicy_NilValue(t *testing.T) {
-	SetTags("db")
+func TestDefaultPolicy_NilValue(t *testing.T) {
+	SetTag("db")
 	SetDefault(nil)
 	SetPolicy(PolicyUseDefault)
 
@@ -87,13 +106,13 @@ func TestDefaultTag_DefaultPolicy_NilValue(t *testing.T) {
 			"visible":  true,
 		},
 		map[string]interface{}{
-			"id":       1,
-			"name":     "item_1",
-			"number":   11,
-			"created":  time.Unix(10000, 0),
+			"id":       2,
+			"name":     "item_2",
+			"number":   22,
+			"created":  time.Unix(20000, 0),
 			"updated":  nil,
 			"discount": nil,
-			"price":    1111.0,
+			"price":    2222.0,
 			"reserved": nil,
 			"points":   nil,
 			"rating":   nil,
@@ -104,8 +123,8 @@ func TestDefaultTag_DefaultPolicy_NilValue(t *testing.T) {
 	doTest(t, getTestItems(), expecteds)
 }
 
-func TestDefaultTag_ExcludePolicy(t *testing.T) {
-	SetTags("db")
+func TestExcludePolicy(t *testing.T) {
+	SetTag("db")
 	SetPolicy(PolicyExclude)
 	SetDefault("SomeDefault")
 
@@ -124,11 +143,11 @@ func TestDefaultTag_ExcludePolicy(t *testing.T) {
 			"visible":  true,
 		},
 		map[string]interface{}{
-			"id":      1,
-			"name":    "item_1",
-			"number":  11,
-			"created": time.Unix(10000, 0),
-			"price":   1111.0,
+			"id":      2,
+			"name":    "item_2",
+			"number":  22,
+			"created": time.Unix(20000, 0),
+			"price":   2222.0,
 			"visible": false,
 		},
 	}
@@ -137,62 +156,30 @@ func TestDefaultTag_ExcludePolicy(t *testing.T) {
 }
 
 func TestCustomTag_DefaultPolicy_DefaultValue(t *testing.T) {
-	SetTags("custom_tag")
+	SetTag("custom_tag")
 	SetDefault("SomeDefault")
 	SetPolicy(PolicyUseDefault)
 
 	expecteds := []map[string]interface{}{
 		map[string]interface{}{
-			"id":           1,
-			"num":          11,
-			"sum":          111,
-			"created_time": time.Unix(10000, 0),
-			"updated_time": mysql.NullTime{time.Unix(11000, 0), true},
-			"is_reserved":  sql.NullBool{true, true},
-			"visible":      true,
+			"id":               1,
+			"num":              11,
+			"sum":              111,
+			"created_time":     time.Unix(10000, 0),
+			"updated_time":     mysql.NullTime{time.Unix(11000, 0), true},
+			"is_reserved":      sql.NullBool{true, true},
+			"visible":          true,
+			"i_ignore_nothing": 10,
 		},
 		map[string]interface{}{
-			"id":           1,
-			"num":          11,
-			"sum":          111,
-			"created_time": "SomeDefault",
-			"updated_time": "SomeDefault",
-			"is_reserved":  "SomeDefault",
-			"visible":      false,
-		},
-	}
-
-	doTest(t, getTestItems(), expecteds)
-}
-
-func TestManyTags_ExcludePolicy_DefaultValue(t *testing.T) {
-	SetPolicy(PolicyExclude)
-	SetTags("db", "custom_tag")
-	SetDefault("SomeDefault")
-
-	expecteds := []map[string]interface{}{
-		map[string]interface{}{
-			"id":           1,
-			"name":         "item_1",
-			"number":       11,
-			"sum":          111,
-			"created_time": time.Unix(10000, 0),
-			"updated":      mysql.NullTime{time.Unix(11000, 0), true},
-			"discount":     111.0,
-			"price":        1111.0,
-			"reserved":     sql.NullBool{true, true},
-			"points":       sql.NullInt64{int64(11), true},
-			"rating":       sql.NullFloat64{1.0, true},
-			"visible":      true,
-		},
-		map[string]interface{}{
-			"id":           1,
-			"name":         "item_1",
-			"number":       11,
-			"sum":          111,
-			"created_time": time.Unix(10000, 0),
-			"price":        1111.0,
-			"visible":      false,
+			"id":               2,
+			"num":              22,
+			"sum":              222,
+			"created_time":     time.Unix(20000, 0),
+			"updated_time":     "SomeDefault",
+			"is_reserved":      "SomeDefault",
+			"visible":          false,
+			"i_ignore_nothing": 20,
 		},
 	}
 
@@ -207,90 +194,29 @@ func doTest(t *testing.T, items []SomeItem, expecteds []map[string]interface{}) 
 	}
 
 	for i, set := range items {
-		m := ToMap(set)
+		m, err := ToMap(set)
+		if err != nil {
+			t.Fatalf("ToMap call returned error: %s", err.Error())
+		}
+
+		// TODO: maybe use just equal asserion here?
+		if len(m) != len(expecteds[i]) {
+			t.Fatalf("size of expected map %d\n%+v\ndoes not match size of generated map %d\n%+v",
+				len(expecteds[i]),
+				expecteds[i],
+				len(m),
+				m)
+		}
 
 		for key, expected := range expecteds[i] {
 			actual, ok := m[key]
 			if !ok {
-				t.Fatalf("could not find key %s in map:\n%v\nexpected map:\n%v", key, actual, m)
+				t.Fatalf("could not find key %s in map:\n%v\nexpected map:\n%v", key, m, expecteds[i])
 			}
-			assert.Equal(t, expected, actual)
+			if !assert.Equal(t, expected, actual) {
+				t.Fatalf("expected value by key %s is %v, got %v", key, expected, actual)
+			}
 		}
-	}
-}
-
-func BenchmarkDefaultPolicy_DefaultValue0(b *testing.B) {
-	SetPolicy(PolicyUseDefault)
-	SetTags("db")
-	SetDefault("SomeDefault")
-
-	items := getTestItems()
-	doBenchmark(b, items[0])
-}
-func BenchmarkDefaultPolicy_DefaultValue1(b *testing.B) {
-	SetPolicy(PolicyUseDefault)
-	SetTags("db")
-	SetDefault("SomeDefault")
-
-	items := getTestItems()
-	doBenchmark(b, items[1])
-}
-
-func BenchmarkExcludePolicy0(b *testing.B) {
-	SetPolicy(PolicyExclude)
-	SetTags("db")
-	SetDefault("SomeDefault")
-
-	items := getTestItems()
-	doBenchmark(b, items[0])
-}
-func BenchmarkExcludePolicy1(b *testing.B) {
-	SetPolicy(PolicyExclude)
-	SetTags("db")
-	SetDefault("SomeDefault")
-
-	items := getTestItems()
-	doBenchmark(b, items[1])
-}
-
-func BenchmarkManyTags_ExcludePolicy0(b *testing.B) {
-	SetPolicy(PolicyExclude)
-	SetTags("db", "custom_tag")
-	SetDefault("SomeDefault")
-
-	items := getTestItems()
-	doBenchmark(b, items[0])
-}
-func BenchmarkManyTags_ExcludePolicy1(b *testing.B) {
-	SetPolicy(PolicyExclude)
-	SetTags("db", "custom_tag")
-	SetDefault("SomeDefault")
-
-	items := getTestItems()
-	doBenchmark(b, items[1])
-}
-
-func BenchmarkManyTags_DefaultPolicy0(b *testing.B) {
-	SetPolicy(PolicyUseDefault)
-	SetTags("db", "custom_tag")
-	SetDefault("SomeDefault")
-
-	items := getTestItems()
-	doBenchmark(b, items[0])
-}
-func BenchmarkManyTags_DefaultPolicy1(b *testing.B) {
-	SetPolicy(PolicyUseDefault)
-	SetTags("db", "custom_tag")
-	SetDefault("SomeDefault")
-
-	items := getTestItems()
-	doBenchmark(b, items[1])
-}
-
-func doBenchmark(b *testing.B, item SomeItem) {
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		ToMap(item)
 	}
 }
 
@@ -298,34 +224,36 @@ func getTestItems() []SomeItem {
 	discount := 111.0
 	return []SomeItem{
 		SomeItem{
-			ID:         1,
-			Name:       "item_1",
-			Number:     11,
-			Checksum:   111,
-			Created:    time.Unix(10000, 0),
-			Updated:    mysql.NullTime{time.Unix(11000, 0), true},
-			Price:      1111.0,
-			Discount:   &discount,
-			IsReserved: sql.NullBool{true, true},
-			Points:     sql.NullInt64{int64(11), true},
-			Rating:     sql.NullFloat64{1.0, true},
-			IsVisible:  true,
-			Notes:      "foo",
+			ID:              1,
+			Name:            "item_1",
+			Number:          11,
+			Checksum:        111,
+			Created:         time.Unix(10000, 0),
+			Updated:         mysql.NullTime{time.Unix(11000, 0), true},
+			Price:           1111.0,
+			Discount:        &discount,
+			IsReserved:      sql.NullBool{true, true},
+			Points:          sql.NullInt64{int64(11), true},
+			Rating:          sql.NullFloat64{1.0, true},
+			IsVisible:       true,
+			Notes:           "foo",
+			SomeIgnoreField: 10,
 		},
 		SomeItem{
-			ID:         1,
-			Name:       "item_1",
-			Number:     11,
-			Checksum:   111,
-			Created:    time.Unix(10000, 0),
-			Updated:    mysql.NullTime{time.Unix(0, 0), false},
-			Price:      1111.0,
-			Discount:   nil,
-			IsReserved: sql.NullBool{true, false},
-			Points:     sql.NullInt64{int64(0), false},
-			Rating:     sql.NullFloat64{1.0, false},
-			IsVisible:  false,
-			Notes:      "bar",
+			ID:              2,
+			Name:            "item_2",
+			Number:          22,
+			Checksum:        222,
+			Created:         time.Unix(20000, 0),
+			Updated:         mysql.NullTime{time.Unix(0, 0), false},
+			Price:           2222.0,
+			Discount:        nil,
+			IsReserved:      sql.NullBool{true, false},
+			Points:          sql.NullInt64{int64(0), false},
+			Rating:          sql.NullFloat64{2.0, false},
+			IsVisible:       false,
+			Notes:           "bar",
+			SomeIgnoreField: 20,
 		},
 	}
 }
