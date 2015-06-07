920bf2ceec9b4b152f5bc891600f4cadf53117f2
elgris <iddqd07@yandex.ru>
2015-06-07 21:58:35 +0700

    - implemented by-struct stom objects. They speed parsing up - global ToMap is still working

--- stom.go
+++ stom.go
@@ -12,109 +12,167 @@ type Policy uint8
 const (
 	PolicyUseDefault Policy = iota
 	PolicyExclude
+)
 
-	defaultTag = "db"
+// package settings
+var (
+	tag          string      = "db"
+	policy       Policy      = PolicyUseDefault
+	defaultValue interface{} = nil
 )
 
 type ToMappable interface {
 	ToMap() (map[string]interface{}, error)
 }
 
-var defaultStom = stom{defaultTag, PolicyUseDefault, nil}
+type ToMapper interface {
+	ToMap(s interface{}) (map[string]interface{}, error)
+}
 
 type stom struct {
-	tag          string
-	policy       Policy
 	defaultValue interface{}
+	policy       Policy
+	tag          string
+
+	typ   reflect.Type
+	cache map[string]int
 }
 
-func (this *stom) SetTag(tag string) {
+func MustNewStom(s interface{}) *stom {
+	typ, err := getStructType(s)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	stom := &stom{
+		typ:          typ,
+		defaultValue: defaultValue,
+		policy:       policy,
+	}
+	return stom.SetTag(tag)
+}
+
+func (this *stom) SetTag(tag string) *stom {
 	this.tag = tag
-}
+	this.cache = extractTagValues(this.typ, this.tag)
 
-func (this *stom) SetDefault(defaultValue interface{}) {
-	this.defaultValue = defaultValue
+	return this
 }
-
-func (this *stom) SetPolicy(policy Policy) {
-	this.policy = policy
-}
+func (this *stom) SetDefault(defaultValue interface{}) { this.defaultValue = defaultValue }
+func (this *stom) SetPolicy(policy Policy)             { this.policy = policy }
 
 func (this *stom) ToMap(s interface{}) (map[string]interface{}, error) {
-	typ := reflect.TypeOf(s)
-	// if a pointer to a struct is passed, get the type of the dereferenced object
-	if typ.Kind() == reflect.Ptr {
-		typ = typ.Elem()
+	val := reflect.ValueOf(s)
+	if val.Kind() == reflect.Ptr {
+		s = val.Elem().Interface()
 	}
+
+	typ := reflect.TypeOf(s)
+	if typ != this.typ {
+		return nil, errors.New(fmt.Sprintf("stom is set up to work with type %s, but %s given", this.typ, typ))
+	}
+
+	return toMap(s, this.cache)
+}
+
+func SetTag(t string)           { tag = t }
+func SetDefault(dv interface{}) { defaultValue = dv }
+func SetPolicy(p Policy)        { policy = p }
+
+func ToMap(s interface{}) (map[string]interface{}, error) {
+	if tomappable, ok := s.(ToMappable); ok {
+		return tomappable.ToMap()
+	}
+
+	typ := reflect.TypeOf(s)
 
 	if typ.Kind() != reflect.Struct {
 		return nil, errors.New(fmt.Sprintf("expected struct, got %v", typ.Kind()))
 	}
 
-	result := map[string]interface{}{}
+	tagmap := extractTagValues(typ, tag)
 
-	val := reflect.ValueOf(s)
-	// loop through the struct's fields and set the map
+	return toMap(s, tagmap)
+}
+
+func getStructType(s interface{}) (t reflect.Type, err error) {
+	t = reflect.TypeOf(s)
+
+	if t.Kind() == reflect.Invalid {
+		err = errors.New(fmt.Sprintf("value is invalid:\n %v", s))
+		return
+	}
+
+	if t.Kind() != reflect.Struct {
+		err = errors.New(fmt.Sprintf("provided value is not a struct!\n%v", s))
+	}
+
+	return
+}
+
+func extractTagValues(typ reflect.Type, tag string) map[string]int {
+	tagValues := map[string]int{}
+
 	for i := 0; i < typ.NumField(); i++ {
 		field := typ.Field(i)
 		if field.PkgPath != "" { // unexported
 			continue
 		}
 
-		// TODO: try indices instead of values
-		// TODO: parallelize it
-		// TODO: cache should work as so:
-		// 1. Create a map 'field name' => index
-		// 2. Get a value by index
-		// 3. Convert value to appropriate value
-		// 4. DONE
+		if tagValue := field.Tag.Get(tag); tagValue != "" && tagValue != "-" {
+			tagValues[tagValue] = i
+		}
+	}
 
-		if t := field.Tag.Get(this.tag); t != "" && t != "-" {
-			vField := val.Field(i)
-			var v interface{}
-			if vField.Kind() == reflect.Ptr {
-				if vField.Elem().IsValid() {
-					v = vField.Elem().Interface()
-				}
-			} else {
-				v = vField.Interface()
-			}
+	return tagValues
+}
 
-			v = convertValue(v)
+func toMap(s interface{}, tagmap map[string]int) (map[string]interface{}, error) {
+	val := reflect.ValueOf(s)
 
-			if v != nil {
-				result[t] = v
-			} else if this.policy == PolicyUseDefault {
-				result[t] = this.defaultValue
-			}
+	result := map[string]interface{}{}
+
+	for tag, index := range tagmap {
+		vField := val.Field(index)
+
+		v, err := convertValue(vField)
+		if err != nil {
+			return result, err
+		}
+
+		if v != nil {
+			result[tag] = v
+		} else if policy == PolicyUseDefault {
+			result[tag] = defaultValue
 		}
 
 	}
 
 	return result, nil
-
-	// TODO:
-	// 1. Check if it's a struct. If not - return error
-	// 2. Scan through struct's fields and get all the tags
-	// 3. If tag is in this.tags - use tag name as a key and field value as value
-	// and put them into map
 }
 
-func SetDefault(defaultValue interface{})                 { defaultStom.SetDefault(defaultValue) }
-func SetTag(tag string)                                   { defaultStom.SetTag(tag) }
-func SetPolicy(policy Policy)                             { defaultStom.SetPolicy(policy) }
-func ToMap(s interface{}) (map[string]interface{}, error) { return defaultStom.ToMap(s) }
-
-func convertValue(input interface{}) (output interface{}) {
-	// TODO: check if input is a structure
-	output = input
-	switch t := input.(type) {
-	case driver.Valuer:
-		if converted, err := t.Value(); converted == nil || err != nil {
-			output = nil
+func convertValue(vField reflect.Value) (v interface{}, err error) {
+	kind := vField.Kind()
+	if kind == reflect.Ptr {
+		if vField.Elem().IsValid() {
+			v = vField.Elem().Interface()
 		}
+	} else {
+		v = vField.Interface()
 	}
 
-	return
+	switch t := v.(type) {
+	case driver.Valuer: // support for NullTypes like sql.NullString and so on
+		if converted, convErr := t.Value(); convErr != nil || converted == nil {
+			v = nil
+		}
+		return
+
+	case ToMappable:
+		v, err = t.ToMap()
+		return
+	}
+
+	return v, nil
 
 }
